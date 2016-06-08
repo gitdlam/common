c2c2b7f5dd87059ac400eeeec4e93f5e91a276af
David Lam <david.lam@staples.com.au>
2016-06-08 19:12:51 +1000

    barcode validation

--- common.go
+++ common.go
@@ -4,17 +4,17 @@ import (
 	"regexp"
 )
 
-func validBarcode8(s string) bool {
+func ValidBarcode8(s string) bool {
 	re := regexp.MustCompile("^[A-Z0-9]{8,8}$")
 	return re.MatchString(s)
 }
 
-func validBarcode7(s string) bool {
+func ValidBarcode7(s string) bool {
 	re := regexp.MustCompile("^[A-Z0-9]{7,7}$")
 	return re.MatchString(s)
 }
 
-func validCarton20(s string) bool {
+func ValidCarton20(s string) bool {
 	re := regexp.MustCompile("^[0-9]{20,20}$")
 	return re.MatchString(s)
 }
