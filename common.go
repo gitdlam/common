a8a2c6c29d14d56c7878ea1366142633712dfbc7
David Lam <david.lam@staples.com.au>
2016-07-25 18:22:06 +1000

    Added EscapeLatex

--- common.go
+++ common.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -37,4 +38,19 @@ func ReverseProxy(proxyPort string, pathMap map[string]string) {
 
 func TimeNowString() string {
 	return fmt.Sprintf("%v", time.Unix(0, time.Now().UnixNano()/(int64(time.Millisecond)/int64(time.Nanosecond))*int64(time.Millisecond)))[:23]
+}
+
+func EscapeLatex(s string) string {
+	s2 := strings.Replace(s, "\\", "\\textbackslash", -1)
+	s2 = strings.Replace(s2, "&", "\\&", -1)
+	s2 = strings.Replace(s2, "%", "\\%", -1)
+	s2 = strings.Replace(s2, "$", "\\$", -1)
+	s2 = strings.Replace(s2, "#", "\\#", -1)
+	s2 = strings.Replace(s2, "_", "\\_", -1)
+	s2 = strings.Replace(s2, "", "\\$", -1)
+	s2 = strings.Replace(s2, "{", "\\{", -1)
+	s2 = strings.Replace(s2, "}", "\\}", -1)
+	s2 = strings.Replace(s2, "~", "\\textasciitilde", -1)
+	return strings.Replace(s2, "^", "\\textasciicircum", -1)
+
 }
